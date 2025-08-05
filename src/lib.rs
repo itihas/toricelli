84c2d0c6dada75238534acf8b5b26af956a13cb7
itihas <sahiti93@gmail.com>
2025-08-05 19:50:58 +0530

    fixup

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,17 @@
-mod config;
-mod score;
-mod types;
+pub mod config;
+pub mod score;
+pub mod types;
 
-use std::collections::HashSet;
 
-use chrono::{DateTime, Utc};
 use config::ToricelliConfig;
-use organic::parser::parse_file;
-use organic::types::{Document, Heading, StandardProperties};
 
 use sqlite::State;
 
-fn fetch_notes(config: ToricelliConfig) {
+pub fn fetch_notes(config: ToricelliConfig) {
     let query = "SELECT id, mtimes, stability, score FROM notes";
 }
 
-fn fetch_notes_from_org_roam_db(config: ToricelliConfig) {
+pub fn fetch_notes_from_org_roam_db(config: ToricelliConfig) {
     let connection = sqlite::open(config.org_roam_db).unwrap();
     let query = "SELECT * FROM \"main\".\"nodes\"";
     let mut statement = connection.prepare(query).unwrap();
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use toricelli::config::ToricelliConfig;
+use toricelli::*;
+
 fn main() {
     println!("Hello, world!");
+    let conf = ToricelliConfig::from_env();
+    fetch_notes_from_org_roam_db(conf);
 }
